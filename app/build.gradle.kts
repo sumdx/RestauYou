de06a2426cb7877093a4024df74c237bedfc3b92
Katominh <jasonandkhangminh@gmail.com>
2025-11-12 00:26:44 -0800

    Added UI's for the Customer Home page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,8 @@ dependencies {
     implementation(libs.constraintlayout)
     implementation(libs.firebase.auth)
     implementation(libs.firebase.firestore)
+    implementation(libs.viewpager2)
+    implementation(libs.recyclerview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- app/src/main/java/com/example/restauyou/UserHomePageActivity.java
+++ app/src/main/java/com/example/restauyou/UserHomePageActivity.java
@@ -7,16 +7,26 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager2.widget.ViewPager2;
 
+import com.example.restauyou.CustomerAdapters.CustomerHomeAdapter;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class UserHomePageActivity extends AppCompatActivity {
-    private FirebaseUser user;
+    FirebaseUser user;
+    Button logoutBtn, cartBtn, menuBtn, reserveBtn;
+    TabLayout tl;
+    ViewPager2 vp2;
+    private String[] tabTitles = {"All", "Pizza", "Burgers", "Pasta", "Salads"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,11 +38,28 @@ public class UserHomePageActivity extends AppCompatActivity {
             return insets;
         });
 
+        // Wiring objects by ids
         user = FirebaseAuth.getInstance().getCurrentUser();
-        TextView displayName = findViewById(R.id.tvDisplayName);
-        displayName.setText(user.getDisplayName());
+        tl = findViewById(R.id.userTabLayout);
+        vp2 = findViewById(R.id.userVP2);
+        cartBtn = findViewById(R.id.cartBtn);
+        menuBtn = findViewById(R.id.menuBtn);
+        reserveBtn = findViewById(R.id.reserveBtn);
+        logoutBtn = findViewById(R.id.demoLogout);
 
-        Button logoutBtn = findViewById(R.id.demoLogout);
+        // This can be useful later on
+        //displayName.setText(user.getDisplayName());
+
+        // Connect TabLayout with ViewPager2
+        vp2.setAdapter(new CustomerHomeAdapter(getSupportFragmentManager(), getLifecycle()));
+        new TabLayoutMediator(tl, vp2, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int i) {
+                tab.setText(tabTitles[i]);
+            }
+        }).attach();
+
+        // Log out function
         logoutBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
