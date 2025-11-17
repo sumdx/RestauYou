c74e3c1807df3ed19b65f8815e9acdc557ab1168
Katominh <jasonandkhangminh@gmail.com>
2025-11-17 14:10:24 -0800

    Made customer home get items from database

--- app/src/main/java/com/example/restauyou/CustomerFragment/CustomerHomeFragment.java
+++ app/src/main/java/com/example/restauyou/CustomerFragment/CustomerHomeFragment.java
@@ -2,10 +2,12 @@ package com.example.restauyou.CustomerFragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,18 +19,27 @@ import com.example.restauyou.CustomerHomePageActivity;
 import com.example.restauyou.ModelClass.MenuFilter;
 import com.example.restauyou.ModelClass.MenuItem;
 import com.example.restauyou.R;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 
 public class CustomerHomeFragment extends Fragment {
     RecyclerView chooseRv, displayRv;
     ImageButton cartBtn;
+    FirebaseFirestore firebaseFirestore;
+    ArrayList<MenuItem> foods = new ArrayList<>();
+    MenuAdapter menuAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_cusomter_home, container, false);
+        firebaseFirestore = FirebaseFirestore.getInstance();
 
         // Initial objects by ids
         chooseRv = view.findViewById(R.id.chooseRecyclerView);
@@ -43,13 +54,10 @@ public class CustomerHomeFragment extends Fragment {
         choose.add(new MenuFilter("Pasta", false));
         choose.add(new MenuFilter("Sushi", false));
 
-        ArrayList<MenuItem> foods = new ArrayList<>();
-        for (int i = 1; i <= 10; i++)
-            foods.add(new MenuItem("Burger " + i, "This is a standard Burger ", "Available", "$99.99", 0, R.drawable.burger_img, false));
-
 
         // Set adapter
-        displayRv.setAdapter(new MenuAdapter(getContext(), foods));
+        menuAdapter = new MenuAdapter(getContext(), foods);
+        displayRv.setAdapter(menuAdapter);
         chooseRv.setAdapter(new MenuFilterAdapter(getContext(), choose));
 
         // Set layout
@@ -60,6 +68,9 @@ public class CustomerHomeFragment extends Fragment {
         llm = new LinearLayoutManager(getContext());
         llm.setOrientation(LinearLayoutManager.VERTICAL);
         displayRv.setLayoutManager(llm);
+
+        // Load items
+        loadMenuItem();
 
         // Cart listener (Using main thread)
         cartBtn.setOnClickListener(new View.OnClickListener() {
@@ -77,5 +88,26 @@ public class CustomerHomeFragment extends Fragment {
         });
 
         return view;
+    }
+
+    private void loadMenuItem() {
+        firebaseFirestore.collection("menu_items").addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                if(error!=null){
+                    Log.d("FirebaseError", error.getMessage());
+                    return;
+                }
+                if(value != null && !value.isEmpty()){
+                    foods.clear();
+                    for(DocumentSnapshot doc: value.getDocuments() ){
+                        MenuItem item = doc.toObject(MenuItem.class);
+                        foods.add(item);
+
+                    }
+                    menuAdapter.setFoods(foods);
+                }
+            }
+        });
     }
 }
