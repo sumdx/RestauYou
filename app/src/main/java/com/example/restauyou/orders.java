027509ddd7be57a62d2af7ec457da53aeae202ac
Raman061 <ramandeepkaur7351302@gmail.com>
2025-12-02 13:41:47 -0800

    commit

--- app/src/main/java/com/example/restauyou/orders.java
+++ app/src/main/java/com/example/restauyou/orders.java
@@ -58,6 +58,6 @@ public class orders extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_orders, container, false);
+        return inflater.inflate(R.layout.fragment_employee_orders, container, false);
     }
 }
