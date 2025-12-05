be70202709af549136948057afeff3c746a13200
Katominh <jasonandkhangminh@gmail.com>
2025-12-05 11:54:26 -0800

    Fixing Customer NOT DONE

--- app/src/main/java/com/example/restauyou/EmployeeHomePageActivity.java
+++ app/src/main/java/com/example/restauyou/EmployeeHomePageActivity.java
@@ -1,6 +1,9 @@
 package com.example.restauyou;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,10 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class EmployeeHomePageActivity extends AppCompatActivity {
+    TextView testText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,18 @@ public class EmployeeHomePageActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        testText = findViewById(R.id.testText);
+
+        testText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Log out
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(EmployeeHomePageActivity.this, LoginActivity.class));
+            }
+        });
     }
+
+
 }
